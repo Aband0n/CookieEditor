68d2771da35204a5e84d07fac4502b1a3a419d27
blackb1rd <blackb1rd.mov@gmail.com>
2020-03-23 23:52:18 +0700

    remove getting start

--- options_pages/options_page_chooser.js
+++ options_pages/options_page_chooser.js
@@ -8,13 +8,6 @@ function setPageCooserEvents() {
     var panel = $(this).attr("id");
     if ($(this).hasClass("selected")) return;
     var id = $(this).attr("id");
-    if (id == "getting_started") {
-      openExtPage("http://www.editthiscookie.com/start/");
-      return;
-    } else if (id == "help") {
-      openExtPage("http://www.editthiscookie.com/faq/");
-      return;
-    }
     ls.set("option_panel", panel);
     location.href = "/options_pages/" + id + ".html";
   });
